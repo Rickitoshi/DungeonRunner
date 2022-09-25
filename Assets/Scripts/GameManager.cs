23ef91370224df99b6558c9dd90d99712967dd36
Rickitoshi <ginko.98@mail.ru>
2022-09-25 16:54:12 +0300

    Add lobby camera

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,27 @@
+using System;
 using DefaultNamespace;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject lobbyCamera;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 120;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
             PlayerController.Instance.State = PlayerState.Run;
+            lobbyCamera.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.O))
         {
             PlayerController.Instance.State = PlayerState.Idle;
+            lobbyCamera.SetActive(true);
         }
     }
 }
